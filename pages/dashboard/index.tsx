576d05c8b47028393627902680cc13a32328a9f1
Ankit Agrawal <ankit@brokermug.com>
2022-05-19 17:07:15 +0530

    Refactoring

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,26 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Header from '@/components/header'
-import Footer from '@/components/footer'
+import Layout from '@/components/Layout'
 import styles from '../../styles/Home.module.css'
 
 const Dashboard: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Ovik Finance - Dashboard</title>
-        <meta name="description" content="Ovik Finance" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header/>
-      <main className={styles.main}>
+    <Layout>
         <h1 className={styles.title}>
           Welcome to <a>Dashboard!</a>
         </h1>
-
-      </main>
-      <Footer/>
-    </div>
+    </Layout>
   )
 }
 
